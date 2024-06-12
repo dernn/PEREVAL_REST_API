00e4e53b6e6c7c262731278209514e960ae613b1
dernn <fajj@mail.ru>
2024-06-12 21:36:16 +0600

    api_level model

--- api/models.py
+++ api/models.py
@@ -24,10 +24,27 @@ class Pereval(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     coords = models.OneToOneField('Coords', on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='new')
-    # level = None
+    level = models.ForeignKey('Level', on_delete=models.CASCADE)
 
 
 class Coords(models.Model):
     latitude = models.DecimalField(decimal_places=8, max_digits=10)
     longitude = models.DecimalField(decimal_places=8, max_digits=10)
     height = models.IntegerField()
+
+
+class Level(models.Model):
+    LEVEL_CHOICE = (
+        ('н/к', 'н/к'),
+        ('1A', '1A'),
+        ('1B', '1B'),
+        ('2A', '2A'),
+        ('2B', '2B'),
+        ('3A', '3A'),
+        ('3B', '3B'),
+    )
+
+    spring = models.CharField(max_length=2, choices=LEVEL_CHOICE, default='н/к')
+    summer = models.CharField(max_length=2, choices=LEVEL_CHOICE, default='н/к')
+    autumn = models.CharField(max_length=2, choices=LEVEL_CHOICE, default='н/к')
+    winter = models.CharField(max_length=2, choices=LEVEL_CHOICE, default='н/к')

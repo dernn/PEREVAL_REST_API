a10adfdc8647acdaf0dd070bb88d572932eb2358
dernn <fajj@mail.ru>
2024-06-12 16:23:37 +0600

    postgresql setup

--- pereval/settings.py
+++ pereval/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'pereval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pereval',
+        'USER': config['FSTR_DB_LOGIN'],
+        'PASSWORD': config['FSTR_DB_PASS'],
+        'HOST': config['FSTR_DB_HOST'],
+        'PORT': config['FSTR_DB_PORT'],
     }
 }
 

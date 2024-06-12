d246cf7f6a8683c3c1c7fd7c06bb8f2cdc996ca7
dernn <fajj@mail.ru>
2024-06-12 20:47:42 +0600

    api_user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    email = models.CharField(max_length=80, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=21)

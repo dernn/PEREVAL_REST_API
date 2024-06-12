fb81877c4ec4255a5d6d13f002b27f4e1d2552e7
dernn <fajj@mail.ru>
2024-06-12 21:20:29 +0600

    api_pereval model

--- api/models.py
+++ api/models.py
@@ -6,3 +6,22 @@ class Users(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=21)
+
+
+class Pereval(models.Model):
+    STATUS_CHOICE = (
+        ('new', 'new'),
+        ('pending', 'pending'),
+        ('accepted', 'accepted'),
+        ('rejected', 'rejected')
+    )
+
+    beauty_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    other_titles = models.CharField(max_length=128)
+    connect = models.CharField(max_length=128)
+    add_time = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    # coords = None
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='new')
+    # level = None

4aed78e3dc131b61b1d2d95f56948ae798ed0e2b
dernn <fajj@mail.ru>
2024-06-14 15:55:59 +0600

    PerevalSerializer create method

--- api/serializers.py
+++ api/serializers.py
@@ -29,4 +29,36 @@ class UsersSerializer(serializers.ModelSerializer):
 
 
 class PerevalSerializer(serializers.ModelSerializer):
-    pass
+    user = UsersSerializer()
+    coords = CoordsSerializer()
+    level = LevelSerializer()
+    images = ImagesSerializer(many=True)
+
+    class Meta:
+        model = Pereval
+        fields = ['beauty_title', 'title', 'other_titles', 'connect',
+                  'add_time', 'user', 'coords', 'level', 'images']
+
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        coords = validated_data.pop('coords')
+        level = validated_data.pop('level')
+        images = validated_data.pop('images')
+
+        current_user = Users.objects.filter(email=user['email'])
+        if current_user.exists():
+            user_serialized = UsersSerializer(data=user)
+            user = user_serialized.save()
+        else:
+            user = Users.objects.create(**user)
+
+        coords = Coords.objects.create(**coords)
+        level = Level.objects.create(**level)
+        pereval = Pereval.objects.create(**validated_data, user=user, coords=coords, level=level)
+
+        for img in images:
+            image = img.pop('images')
+            title = img.pop('title')
+            Images.objects.create(image=image, title=title, pereval=pereval)
+
+        return pereval

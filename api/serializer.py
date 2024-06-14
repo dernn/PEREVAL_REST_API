0cc7315e1a8b668dcaa97ff19f094457edecd063
dernn <fajj@mail.ru>
2024-06-14 13:11:43 +0600

    serializers added [Coords, Level, Images, Users]

---
+++ api/serializer.py
@@ -0,0 +1,28 @@
+from .models import Coords, Level, Pereval, Images, Users
+from rest_framework import serializers
+
+
+class CoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ['latitude', 'longitude', 'height']
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = ['spring', 'summer', 'autumn', 'winter']
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+    data = serializers.URLField()
+
+    class Meta:
+        model = Images
+        fields = ['image', 'title']
+
+
+class UsersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['email', 'first_name', 'last_name', 'phone']

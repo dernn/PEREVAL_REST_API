1be298c5500d784f2383dba74ce74cbda1dbdd5a
dernn <fajj@mail.ru>
2024-06-14 13:46:44 +0600

    api.models register

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import Pereval, Users, Coords, Level, Images
 
-# Register your models here.
+admin.site.register(Users)
+admin.site.register(Pereval)
+admin.site.register(Coords)
+admin.site.register(Level)
+admin.site.register(Images)

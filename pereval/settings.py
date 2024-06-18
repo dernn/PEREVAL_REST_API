09c951165dfd661b5b14e4caab03a4fe52ad2e33
dernn <fajj@mail.ru>
2024-06-18 13:31:28 +0600

    ALLOWED_HOSTS setup

--- pereval/settings.py
+++ pereval/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config['DEBUG_MODE']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dernn.pythonanywhere.com']
 
 
 # Application definition

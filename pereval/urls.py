39f07c535e43f76d11ddb423c42bdae6f85f0d65
dernn <fajj@mail.ru>
2024-06-19 13:59:09 +0600

    urls comment

--- pereval/urls.py
+++ pereval/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', lambda request: redirect('api/', permanent=False)),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # to download .yaml
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
